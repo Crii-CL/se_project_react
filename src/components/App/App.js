32cacb63b95841b4380ccdf3b86e916a10b6f949
Crii-CL <crisrecon14@gmail.com>
2023-04-07 06:41:47 -0400

    fixed rendering issues

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,25 @@
-import logo from "../../../public/images/logo.svg";
-import "./App.css";
-import ItemModal from "../ItemCard/ItemModal";
+import "../../blocks/App.css";
+// import ItemModal from "../ItemCard/ItemModal";
 import React from "react";
 
 function App() {
   return (
-    <div>
-      <header className="header">
-        <div className="header__logo">wtwr°</div>
-        <div className="header__text">date</div>
-        <button className="header__button" type="text">
-          Add New Clothes
-        </button>
-        <div className="header__text">Welcome</div>
-        <div className="header__text">Cristopher Campos</div>
-      </header>
-    </div>
+    <>
+      <div>
+        <header className="header">
+          <div className="header__logo">wtwr°</div>
+          <div className="header__text">date</div>
+          <button className="header__button" type="text">
+            Add New Clothes
+          </button>
+          <div className="header__text">Welcome</div>
+          <div className="header__text">Cristopher Campos</div>
+          <div className="header__avatar">
+            <img className="header__avatar-image"></img>
+          </div>
+        </header>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./blocks/index.css";
-// import "./components/App";
+import App from "./components/App/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
