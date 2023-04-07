431702d04f8927820ec143e2f145fe3be8affdae
Crii-CL <crisrecon14@gmail.com>
2023-04-07 19:00:43 -0400

    still working on the header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import "../../blocks/App.css";
+import avatar from "../../images/avatar.png";
 import logo from "../../images/logo.svg";
 // import ItemModal from "../ItemCard/ItemModal";
 import React from "react";
@@ -13,17 +14,18 @@ function App() {
     <>
       <div>
         <header className="header">
-          <div className="header__logo">wtwr°</div>
+          <div className="header__logo">
+            <img src={logo} alt="wtwr logo"></img>
+          </div>
           <div className="header__text">date</div>
           <button className="header__button" type="text">
-            Add New Clothes
+            +Add New Clothes
           </button>
-          <div className="header__text">Welcome</div>
           <div className="header__name">Cristopher Campos</div>
           <div className="header__avatar">
             <img
               className="header__avatar-image"
-              src="{logo}"
+              src={avatar}
               alt="avatar logo"
             ></img>
           </div>
