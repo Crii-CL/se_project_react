c7e7acc78e2f9122c9fdbea541ed51e2ba83b8c3
Crii-CL <crisrecon14@gmail.com>
2023-04-07 21:20:39 -0400

    begin working on Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,51 +1,15 @@
-import "../../blocks/App.css";
-import avatar from "../../images/avatar.png";
-import logo from "../../images/logo.svg";
+import Header from "../Header/Header";
 // import ItemModal from "../ItemCard/ItemModal";
 import React from "react";
-import { useEffect } from "react";
 
 // const currentDateEl = document.getElementById('currentDate');
 // currentDateEl.innerHTML = currentDate;
 
 function App() {
-  const currentDate = new Date().toLocaleString("default", {
-    month: "long",
-    day: "numeric",
-  });
-
-  useEffect(() => {
-    const currentDateElement = document.createElement("div");
-    currentDateElement.id = "current-date";
-    currentDateElement.innerHTML = currentDate;
-  }, [currentDate]);
-
   return (
     <>
       <div>
-        <header className="header">
-          <div className="header__container" id="logo-date">
-            <div className="header__logo">
-              <img src={logo} alt="wtwr logo"></img>
-            </div>
-            <div className="header__text" id="currentDate">
-              {currentDate}
-            </div>
-          </div>
-          <div className="header__container" id="button-name-avatar">
-            <button className="header__button" type="text">
-              + Add New Clothes
-            </button>
-            <div className="header__name">Cristopher Campos</div>
-            <div className="header__avatar">
-              <img
-                className="header__avatar-image"
-                src={avatar}
-                alt="avatar logo"
-              ></img>
-            </div>
-          </div>
-        </header>
+        <Header />
       </div>
     </>
   );
