4f3f879c9a4552c9a8e8c3b0ab1e5c772303cd78
Crii-CL <crisrecon14@gmail.com>
2023-04-10 23:01:20 -0400

    added the default clothing items array

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,11 +16,43 @@ import sneakers from "../../images/Sneakers 1.svg";
 import tshirt from "../../images/T-Shirt.svg";
 /* ----------------------------- ^Item Imports^ ----------------------------- */
 
-const cardImagesArray = [
-  { url: cap, name: "Cap" },
-  { url: shorts, name: "Shorts" },
-  { url: sneakers, name: "Sneakers" },
-  { url: tshirt, name: "T-Shirt" },
+const defaultClothingItems = [
+  {
+    _id: 0,
+    name: "Cap",
+    weather: "hot",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591",
+  },
+  {
+    _id: 1,
+    name: "Hoodie",
+    weather: "warm",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Hoodie.png?etag=5f52451d0958ccb1016c78a45603a4e8",
+  },
+  {
+    _id: 2,
+    name: "Jacket",
+    weather: "cold",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Jacket.png?etag=f4bb188deaa25ac84ce2338be2d404ad",
+  },
+  {
+    _id: 3,
+    name: "Sneakers",
+    weather: "cold",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sneakers.png?etag=3efeec41c1c78b8afe26859ca7fa7b6f",
+  },
+  {
+    _id: 4,
+    name: "T-Shirt",
+    weather: "hot",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
+  },
+  {
+    _id: 5,
+    name: "Winter coat",
+    weather: "cold",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
+  },
 ];
 
 export default function App() {
@@ -53,12 +85,12 @@ export default function App() {
           <Weather day={false} type="clear" />
           <section className="cards" id="card-section">
             <ul className="cards__list" id="card-list">
-              {cardImagesArray.map((card, index) => (
+              {defaultClothingItems.map((card, index) => (
                 <Cards
                   key={_.uniqueId(card.name)}
                   handleCardClick={handleCardClick}
                   name={card.name}
-                  url={card.url}
+                  url={card.link}
                 />
               ))}
             </ul>
