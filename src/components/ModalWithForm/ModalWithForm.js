f965d7f47ecdecd365e85a7bf2ee180ea151b1fd
Crii-CL <crisrecon14@gmail.com>
2023-04-10 21:10:05 -0400

    still working on modal with form

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,15 +1,18 @@
 import "./ModalWithForm.css";
+import closeButton from "../../images/close-button.svg";
 
 export default function ModalWithForm() {
   return (
     <>
       <div className="formModal">
         <form className="formModal__form">
-          <button className="formModal__close" type="button"></button>
+          <button className="formModal__close" type="button">
+            <img src={closeButton}></img>
+          </button>
           <h1 className="formModal__title" id="new-garment">
-            New Garment
+            New Garment:
           </h1>
-          <fieldset className="formModal_fieldset">
+          <fieldset className="formModal__fieldset">
             <p className="formModal__caption">Name</p>
             <input className="formModal__input" placeholder="Name"></input>
             <p className="formModal__caption">Image</p>
@@ -18,7 +21,7 @@ export default function ModalWithForm() {
           <h1 className="formModal__title" id="weather-type">
             Select the weather type:
           </h1>
-          <fieldset className="formModal__btn-container">
+          <fieldset className="formModal__fieldset">
             <button type="radio" className="formModal__btn-radio"></button>
             <button type="radio" className="formModal__btn-radio"></button>
             <button type="radio" className="formModal__btn-radio"></button>
