2a57dc523207e4186c236ecd0a848702d0a6afab
Crii-CL <crisrecon14@gmail.com>
2023-04-21 00:31:54 -0400

    .

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -2,17 +2,15 @@ import "../Checkbox/Checkbox.css";
 
 export default function Checkbox({ name, isChecked, handleChange }) {
   return (
-    <div className="checkbox">
-      <label className="checkbox__label">
-        <input
-          className="checkbox__box"
-          type="checkbox"
-          name={name}
-          isChecked={isChecked}
-          onChange={handleChange}
-        ></input>
-        <span className="checkbox__slider"></span>
-      </label>
-    </div>
+    <label className="checkbox">
+      <input
+        className="checkbox__box"
+        type="checkbox"
+        name={name}
+        isChecked={isChecked}
+        onChange={handleChange}
+      ></input>
+      <span className="checkbox__slider"></span>
+    </label>
   );
 }
