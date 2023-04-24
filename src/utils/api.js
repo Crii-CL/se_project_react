8ecc1ce7757fb53fd4ca8d5023d48d3443e28a41
Crii-CL <crisrecon14@gmail.com>
2023-04-24 08:41:23 -0400

    created itemsAPI need to test it now

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,54 @@
 const baseUrl = "https://localhost:3000";
 
-return fetch(`${baseUrl}/items`)
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Error${res.status}`);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+export default function itemsApi() {
+  return {
+    get: () => {
+      return fetch(`${baseUrl}/items`)
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(`Error${res.status}`);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    add: (name, imageUrl, weather) => {
+      return fetch(`${baseUrl}/items`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          imageUrl,
+          weather,
+        }),
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(`Error${res.status}`);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    delete: () => {
+      return fetch(`${baseUrl}/items/:id`, {
+        method: "DELETE",
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(`Error ${res.status}`);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  };
+}
