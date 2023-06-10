e4de5d1329d4327ea410512c374d30c48e5dff8d
Crii-CL <crisrecon14@gmail.com>
2023-06-09 20:07:19 -0400

    added like buttons

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -7,8 +7,12 @@ export default function ItemCard({ handleCardClick, name, url, weather, id }) {
       className="cards__el"
       onClick={() => handleCardClick(name, url, weather, id)}
     >
-      <p className="cards__caption">{name}</p>
-      <button className="cards__button" src={likeButton}></button>
+      <div className="cards__caption">
+        <p className="cards__name">{name}</p>
+        <button className="cards__button">
+          <img src={likeButton} />
+        </button>
+      </div>
       <img className="cards__image" src={url} alt={name}></img>
     </li>
   );
