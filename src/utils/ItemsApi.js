b1c288f0d9f905003e2f9e873c97366f05f0a1f3
Crii-CL <crisrecon14@gmail.com>
2023-10-03 17:45:00 -0400

    changed site names

--- src/utils/ItemsApi.js
+++ src/utils/ItemsApi.js
@@ -3,7 +3,7 @@ import checkResponse from "../utils/api";
 export const itemsApi = (currentUser) => {
   const baseUrl =
     process.env.NODE_ENV === "production"
-      ? "https://soft-starburst-848c30.netlify.app"
+      ? "https://wtwr-back.netlify.app"
       : "http://localhost:3001";
 
   const token = localStorage.getItem("jwt");
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,7 +3,7 @@ import checkResponse from "../utils/api";
 const SignupOrSignin = () => {
   const baseUrl =
     process.env.NODE_ENV === "production"
-      ? "https://soft-starburst-848c30.netlify.app"
+      ? "https://wtwr-back.netlify.app"
       : "http://localhost:3001";
 
   return {
